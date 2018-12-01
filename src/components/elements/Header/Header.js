1f22f30eca073604bd78409f9327d2c2414a07ee
Oscar Sanchez <ojsm45@gmail.com>
2018-12-01 17:38:22 -0300

    adding Header and Home components

---
+++ src/components/elements/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return(
+    <div className="rmdb-header">
+      <div className="rmdb-header-content">
+        <img className="rmdb-logo" src="" alt="rmdb-logo"/>
+        <img className="rmdb-tmdb-logo" src="" alt="tmdb-logo"/>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
