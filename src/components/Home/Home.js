9945a2f8f49de419a97076baeaf78042478017cd
Oscar Sanchez <ojsm45@gmail.com>
2018-12-01 19:05:01 -0300

    adding elements w. styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import './Home.css';
 
+import HeroImage from '../elements/HeroImage/HeroImage';
+import SearchBar from '../elements/SearchBar/SearchBar';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import MovieThumb from '../elements/MovieThumb/MovieThumb';
+import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
+import Spinner from '../elements/Spinner/Spinner';
+
 class Home extends Component {
   state = {
 
@@ -9,7 +16,12 @@ class Home extends Component {
   render(){
     return(
       <div>
-        Home
+        <HeroImage />
+        <SearchBar />
+        <FourColGrid />
+        <MovieThumb />
+        <LoadMoreBtn />
+        <Spinner />
       </div>
     );
   };
