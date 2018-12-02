720943853d7a63abccc35045463b86a9c16ba5db
Oscar Sanchez <ojsm45@gmail.com>
2018-12-02 19:02:08 -0300

    add conditional link on movie thumb

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
   return(
     <div className="rmdb-header">
       <div className="rmdb-header-content">
-        <Link>
-          <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/>
+        <Link to="/">
+          <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/> 
         </Link>
         <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo"/>
       </div>
--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './MovieThumb.css';
 
 const MovieThumb = (props) => {
   return(
     <div className="rmdb-moviethumb">
-      <img src={props.image} alt="movie-thumb" />
+      { props.clickeableb ? 
+        <Link to={{ pathname: `/${props.movieId}`, movieName: `${props.movieName}` }}>
+          <img src={props.image} alt="movie-thumb" />
+        </Link>
+      :
+        <img src={props.image} alt="movie-thumb" />
+      }
     </div>
   );
 };
