33c34dc8aaeebb76975143b11c485bda4c9d76af
Oscar Sanchez <ojsm45@gmail.com>
2018-12-02 02:34:26 -0300

    adding config api, url, endpoint. Updating state, compDidMount method and fetchItems methods

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './Home.css';
+
+import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../../config';
 
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
@@ -7,10 +8,55 @@ import FourColGrid from '../elements/FourColGrid/FourColGrid';
 import MovieThumb from '../elements/MovieThumb/MovieThumb';
 import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
 import Spinner from '../elements/Spinner/Spinner';
+import './Home.css';
 
 class Home extends Component {
   state = {
+    // To collect the movies
+    movies: [],
+    // For Big Image size
+    heroImage: null,
+    // To check if we are loading
+    loading: false,
+    // For pager
+    currentPage: 0,
+    totalPages: 0,
+    // For search movie
+    searchTerm: ''
+  }
 
+  componentDidMount(){
+    this.setState({loading: true});
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  };
+
+  loadMoreItems = () => {
+    let endpoint = '';
+    this.setState({ loading: true });
+
+    if(this.state.searchTerm === ''){
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+    } else{
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=es-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
+  };
+
+  fetchItems = (endpoint) => {
+    fetch( endpoint )
+    .then( result => result.json() )
+    .then( result => {
+      // Here we must have the data in json format
+      this.setState({
+        // Using ES6 Spread operator to take out array elements 
+        movies: [...this.state.movies, ...result.results],
+        heroImage: this.state.heroImage || result.results[0],
+        loading: false,
+        currentPage: result.page,
+        totalPages: result.total_pages
+      });
+    });
   }
 
   render(){
@@ -19,9 +65,8 @@ class Home extends Component {
         <HeroImage />
         <SearchBar />
         <FourColGrid />
-        <MovieThumb />
-        <LoadMoreBtn />
         <Spinner />
+        <LoadMoreBtn />
       </div>
     );
   };
---
+++ src/config.js
@@ -0,0 +1,26 @@
+// Configuration for TMDB
+// To see the latest configuration fetch ir from:
+// https://API_CONFIG_URL
+
+const API_URL = 'API_URL';
+const API_KEY = 'API_KEY';
+
+// Images
+// An image URL looks like this example:
+// URL_API_SITE
+
+const IMAGE_BASE_URL = 'IMAGE_BASE_URL';
+
+// Sizes: w300, w780, w1280, original
+const BACKDROP_SIZE = 'w1280';
+
+// w92, w154, w185, w342, w500, w780, original
+const POSTER_SIZE = 'w500';
+
+export {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE
+}
