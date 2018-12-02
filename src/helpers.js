198391a56b08fc9fdbf6bfab3f9af91f38e637a4
Oscar Sanchez <ojsm45@gmail.com>
2018-12-02 19:49:44 -0300

    add scaffold for movie page

---
+++ src/helpers.js
@@ -0,0 +1,16 @@
+// Convert to hours and minutes
+export const calcTime = (time) => {
+  const hours = Math.floor(time / 60);
+  const mins = time % 60;
+  return `${hours}h ${mins}m`;
+};
+
+// Number on currency $usd format
+export const convertMoney = ( money ) => {
+  var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+  });
+  return formatter.format( money );
+};
