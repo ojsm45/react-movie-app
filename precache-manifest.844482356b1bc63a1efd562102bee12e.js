6aa6e36238d403fc41d6d310d4f7713611090eca
Oscar Sanchez <ojsm45@gmail.com>
2018-12-11 23:10:47 -0300

    Updates

---
+++ precache-manifest.844482356b1bc63a1efd562102bee12e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdcfacafbc62971f5461",
+    "url": "/react-movie-app/static/js/runtime~main.bdcfacaf.js"
+  },
+  {
+    "revision": "5c8f73f27f3eaccf874a",
+    "url": "/react-movie-app/static/js/main.5c8f73f2.chunk.js"
+  },
+  {
+    "revision": "fba6849f7ea4e2ade5fb",
+    "url": "/react-movie-app/static/js/1.fba6849f.chunk.js"
+  },
+  {
+    "revision": "5c8f73f27f3eaccf874a",
+    "url": "/react-movie-app/static/css/main.020f60f5.chunk.css"
+  },
+  {
+    "revision": "39f6a50974c3f9bd9a4e2cdebf153ba0",
+    "url": "/react-movie-app/index.html"
+  }
+];
