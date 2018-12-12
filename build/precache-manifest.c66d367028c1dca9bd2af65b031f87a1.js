46abed479443d9d2e992fac5a65305b32f7019d9
Oscar Sanchez <ojsm45@gmail.com>
2018-12-11 22:57:04 -0300

    Initial build subtree commit

---
+++ build/precache-manifest.c66d367028c1dca9bd2af65b031f87a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c8f73f27f3eaccf874a",
+    "url": "/static/js/main.5c8f73f2.chunk.js"
+  },
+  {
+    "revision": "fba6849f7ea4e2ade5fb",
+    "url": "/static/js/1.fba6849f.chunk.js"
+  },
+  {
+    "revision": "5c8f73f27f3eaccf874a",
+    "url": "/static/css/main.020f60f5.chunk.css"
+  },
+  {
+    "revision": "4803a4f08cd19f2851ced1c199785438",
+    "url": "/index.html"
+  }
+];
