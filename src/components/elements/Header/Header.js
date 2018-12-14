ef90da7e39fa179be44b5ed0a4454737e86017c0
Oscar Sanchez <ojsm45@gmail.com>
2018-12-13 23:37:21 -0300

    you know

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
 import './Header.css';
 
 const Header = () => {
   return(
     <div className="rmdb-header">
       <div className="rmdb-header-content">
-        <Link to={process.env.PUBLIC_URL + '/react-movie-app/'}>
-          <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/> 
-        </Link>
+        <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/> 
         <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo"/>
       </div>
     </div>
